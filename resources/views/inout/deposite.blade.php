31233f07735c2c33525340dcd96d7dd617fc8574
Justin True <justin.d.true@wmich.edu>
2017-12-27 18:11:27 -0500

    change input layout

--- resources/views/inout/deposite.blade.php
+++ resources/views/inout/deposite.blade.php
@@ -15,22 +15,33 @@
 	<form method="post" action="{{route('deposite')}}">
 		{{ csrf_field()}}
 		<input type="hidden" name="account" value="{{$account->id}}">
-		Date
-		<input type='date' class='form-control' name='date'>
-		Amount
-		<input type='number' class='form-control' name='amount' step="0.01">
-		Catagory
-		<select class='form-control' name='catagory'>
-			@foreach(explode(',', $account->catagories) as $catagory)
-				<option>{{$catagory}}</option>
-			@endforeach
-		</select>
-		Store
-		<input type='text' class='form-control' name='store'>
+		<div class="row">
+			<div class="col-md-3">
+				Date
+				<input type='date' class='form-control' name='date'>
+			</div>
+			<div class="col-md-2">
+				Amount
+				<input type='number' class='form-control' name='amount' step="0.01">
+			</div>
+			<div class="col-md-3">
+				Catagory
+				<select class='form-control' name='catagory'>
+					@foreach(explode(',', $account->catagories) as $catagory)
+						<option>{{$catagory}}</option>
+					@endforeach
+				</select>
+			</div>
+			<div class="col-md-4">
+				Store
+				<input type='text' class='form-control' name='store'>
+			</div>
+		</div>
 		Description
-		<input type='text' class='form-control' name='description'>
+		<textarea class='form-control' name='description'></textarea>
 		Receipt
 		<input type='file' class='form-control' name='receipt'>
+		<br/>
 		<button type="submit" class="btn tbn-info">Add</button>
 	</form>
 @endsection
